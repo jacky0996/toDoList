de3169e388738deb8c17abe532b2f3e8a594d66c
ShaneLin <shane_lin@digeam.com>
2024-06-03 17:13:00 +0800

    v0.1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'login' }">TodoList</RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+	<script setup>
+import { reactive } from 'vue'
+
+const user = reactive({
+  username: '',
+  password: ''
+})
+
+const onLogin = () => {
+  const { username, password } = user
+  console.log('onLogin: ', username, password)
+}
+</script>
+ 
+<template>
+  <div class="mx-4">
+    <form @submit.prevent="onLogin">
+      <div class="grid grid-cols-[100px_1fr] gap-2 items-center mx-4">
+        <label for="username-input">帳號: </label>
+        <input
+          class="border-b-4 border-gray-700 rounded-sm p-2"
+          id="username-input"
+          type="text"
+          v-model="user.username"
+        />
+
+        <label for="password-input" class="ml-10">密碼: </label>
+        <input
+          class="border-b-4 border-gray-700 rounded-sm p-2"
+          id="password-input"
+          type="text"
+          v-model="user.password"
+        />
+      </div>
+
+      <div class="text-center mt-2">
+        <button
+          class="bg-green-500 text-white px-4 py-2 rounded-sm transition hover:brightness-105"
+        >
+          登入
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
